48301fd56a9d8b343469f69decd637649986116f
web@indoeuropean <web@indoeuropean.in>
2025-07-01 12:16:12 +0530

    seo issues fixing

--- Program.cs
+++ Program.cs
@@ -13,12 +13,82 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+app.Use(async (context, next) =>
+{
+    if (context.Request.Path == "/home/index")
+    {
+        context.Response.Redirect("/", permanent: true);
+        return;
+    }
+    await next();
+});
+
+app.Use(async (context, next) =>
+{
+    var request = context.Request;
+
+    // Normalize path (lowercase + no /home/index)
+    var path = request.Path.Value;
+
+    if (path != null)
+    {
+        var normalizedPath = path.ToLowerInvariant();
+
+        if (normalizedPath == "/home/index")
+        {
+            context.Response.Redirect("/", true);
+            return;
+        }
+
+        if (normalizedPath != path)
+        {
+            context.Response.Redirect(normalizedPath, true);
+            return;
+        }
+    }
+
+    await next();
+});
+
+app.Use(async (context, next) =>
+{
+    context.Response.Headers.Append("Content-Security-Policy",
+        "default-src 'self'; " +
+        "script-src 'self' https://www.youtube.com https://www.google.com https://www.gstatic.com https://kit.fontawesome.com; " +
+        "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://cdnjs.cloudflare.com https://kit.fontawesome.com; " +
+        "font-src 'self' https://fonts.gstatic.com https://cdnjs.cloudflare.com https://kit.fontawesome.com; " +
+        "img-src 'self' data: https://i.ytimg.com; " +
+        "frame-src https://www.youtube.com https://www.youtube-nocookie.com;"
+    );
+    await next();
+});
+
+
+
+// Custom middleware to enforce lowercase URLs
+app.Use(async (context, next) =>
+{
+    var path = context.Request.Path.Value;
+    var queryString = context.Request.QueryString.Value;
+
+    if (!string.IsNullOrEmpty(path) && path != path.ToLowerInvariant())
+    {
+        var lowercaseUrl = path.ToLowerInvariant() + queryString;
+        context.Response.Redirect(lowercaseUrl, permanent: true);
+        return;
+    }
+
+    await next();
+});
+
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
 
 app.UseAuthorization();
+app.MapControllers();
 
 app.MapControllerRoute(
     name: "default",
