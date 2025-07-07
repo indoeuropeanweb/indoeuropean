1b20cab4f8d6a08a2551fa11e4b4a349e63998f9
web@indoeuropean <web@indoeuropean.in>
2025-07-07 14:06:49 +0530

    get ireland form

--- obj/Debug/net8.0/indoeuropean.AssemblyInfo.cs
+++ obj/Debug/net8.0/indoeuropean.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("indoeuropean")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f559779df83f9ee95f50f1ee49ae5d15002f634")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15ededfa4917f2894c241253d40bfe9547842a3c")]
 [assembly: System.Reflection.AssemblyProductAttribute("indoeuropean")]
 [assembly: System.Reflection.AssemblyTitleAttribute("indoeuropean")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
