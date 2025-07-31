8b9dc6c25500ad82349ecf37eb4cfddc7c4e0d5d
web@indoeuropean <web@indoeuropean.in>
2025-07-31 11:30:53 +0530

    changes to git

--- obj/Debug/net8.0/indoeuropean.AssemblyInfo.cs
+++ obj/Debug/net8.0/indoeuropean.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("indoeuropean")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91e85fd4228fe9f4df5a4fd3dc7823b4b6606c05")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b60109f5fb48447d4137e7c94d70d96e465029bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("indoeuropean")]
 [assembly: System.Reflection.AssemblyTitleAttribute("indoeuropean")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
