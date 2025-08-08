272d3eadc5d13ed49e53e62a5a9e3243528e3a46
web@indoeuropean <web@indoeuropean.in>
2025-08-08 18:21:49 +0530

    designed associate agents page with theme

--- obj/Debug/net8.0/indoeuropean.AssemblyInfo.cs
+++ obj/Debug/net8.0/indoeuropean.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("indoeuropean")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64ae7ba20b2f6d5c77d68bca49c84eb6dd2393fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fde1e07eed026cecaf46e2472363b54a94e6d074")]
 [assembly: System.Reflection.AssemblyProductAttribute("indoeuropean")]
 [assembly: System.Reflection.AssemblyTitleAttribute("indoeuropean")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
